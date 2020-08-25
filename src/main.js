73eb8e05b151796ae3e9a8d4143d34244d3f3013
sangtaek lee <srs99125@naver.com>
2020-08-25 22:18:18 +0900

    geolocation 받아오는 위치, 타이밍 수정
    1. 기존에 사이드바에서 콜백으로 geolocation을 받아오던 부분을 지우고 main.js에서 데이터를 받아온 후 App을 실행하는 것으로 수정

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-new App(document.querySelector("#App"));
+const main = (loc) => {
+  new App(document.querySelector("#App"), loc);
+}
+
+if('geolocation' in navigator) {  
+  navigator.geolocation.getCurrentPosition(pos => main({
+    x: pos.coords.latitude,
+    y: pos.coords.longitude,
+  }))
+} else {
+  main({
+    x: 37.395356858155154,
+    y: 127.0900521371181,
+  })
+}
