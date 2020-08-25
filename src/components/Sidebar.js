9e51ed45026105a31911e60f53fba6550a62d3e8
sangtaek lee <srs99125@naver.com>
2020-08-25 22:24:32 +0900

    sidebar.js 수정
    1. 클릭된 placeInfo의 아이디를 가르키는 clicked 변수 추가
    2. sidebar에 이벤트 위임을 통한 이벤트 리스터를 추가하고 콜백함수를 통해 app.clickedPlace를 수정하는 기능 추가
    3. 클릭된 placeInfo는 노랑색 배경화면이 되도록 js로직 및 css 적용

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,21 @@
 class Sidebar {
-  constructor({ $app, placeList }) {
+  constructor({ $app, placeList, updateAppClickedPlace }) {
     this.$app = $app;
     this.sidebar = document.createElement('div');
     this.sidebar.className = 'sidebar-wrapper';
+    this.clicked = null;
+    this.updateAppClickedPlace = updateAppClickedPlace;
+
+    this.sidebar.addEventListener('click', (e) => {
+      const place = this.state.placeList.find(place => place.id === e.target.parentNode.id);
+      if (place) {
+        // app.state.clickedPlace를 변경
+        this.updateAppClickedPlace({place, target: 'map'});
+        // clicked를 변경
+        this.setPlaceClicked(place);
+      }
+    })
+
     this.$app.appendChild(this.sidebar);
 
     this.state = {
@@ -16,7 +29,7 @@ class Sidebar {
     this.sidebar.innerHTML = `
       ${this.state.placeList
         .map(place => 
-          `<div class="placeInfo" id="${place.id}">
+          `<div class="${this.clicked === place.id ? 'placeInfo clicked' : 'placeInfo'}" id="${place.id}">
             <div>이름: ${place.place_name}</div>
             <div>연락처: ${place.phone ? place.phone : '-'}</div>
             <div>주소: ${place.address_name}</div>
@@ -33,5 +46,12 @@ class Sidebar {
       placeList: newPlaceList
     }
     this.render();
+  }
+  setPlaceClicked(place) {
+    if (this.clicked) {
+      document.getElementById(this.clicked).classList.remove('clicked');
+    }
+    this.clicked = place.id;
+    document.getElementById(this.clicked).classList.add('clicked');
   }
 }
